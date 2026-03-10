9e892e51fa4e7006aa02e8d4486c1b264bd183ed
Josep Audenis <pepaudenisr@gmail.com>
2026-03-10 17:49:33 +0100

    softmax categorical cross-entropy loss

---
+++ src/loss/softmax_categorical_cross_entropy.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from activation.softmax import SoftmaxActivation
+from loss.categorical_cross_entropy import CategoricalCrossEntropyLoss
+
+class SoftmaxCategoricalCrossEntropyAcitvationLoss():
+
+    def __init__(self):
+        self.activation = SoftmaxActivation()
+        self.loss = CategoricalCrossEntropyLoss()
+
+
+    def forward(self, inputs, y_true):
+        self.activation.forward(inputs)
+        self.output = self.activation.output
+        return self.loss.calculate(self.output, y_true)
+    
+    
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+
+        if len(y_true.shape == 2):
+            y_true = np.argmax(y_true, axis=1)
+
+        self.dinputs = dvalues.copy()
+        self.dinputs[range(samples), y_true] -= 1
+        self.dinputs = self.dinputs / samples
