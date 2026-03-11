6fba7bb49b9b8bef65733e24d41d64410651912e
Josep Audenis <pepaudenisr@gmail.com>
2026-03-11 15:16:47 +0100

    dense layer and dropout

---
+++ src/layer/dense.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+class LayerDense:
+
+    def __init__(self, n_inputs, n_neurons, weight_regularizer_l1=0, weight_regularizer_l2=0, bias_regularizer_l1=0, bias_regularizer_l2=0):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+        self.weight_regularizer_l1 = weight_regularizer_l1
+        self.weight_regularizer_l2 = weight_regularizer_l2
+        self.bias_regularizer_l1 = bias_regularizer_l1
+        self.bias_regularizer_l2 = bias_regularizer_l2
+        
+
+    def forward(self, inputs, training):
+        self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        if self.weight_regularizer_l1 > 0:
+            dl1 = np.ones_like(self.weights)
+            dl1[self.weights < 0] = -1
+            self.dweights += self.weight_regularizer_l1 * dl1
+        
+        if self.weight_regularizer_l2 > 0:
+            self.dweights += 2 * self.weight_regularizer_l2 * self.weights
+
+        if self.bias_regularizer_l1 > 0:
+            dl1 = np.ones_like(self.biases)
+            dl1[self.biases < 0] = -1
+            self.biases += self.bias_regularizer_l1 * dl1
+        
+        if self.bias_regularizer_l2 > 0:
+            self.dbiases += 2 * self.bias_regularizer_l2 * self.biases
+
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
+    
+    def get_parameters(self):
+        return self.weights, self.biases
+    
+
+    def set_parameters(self, weights, biases):
+        self.weights = weights
+        self.biases = biases
